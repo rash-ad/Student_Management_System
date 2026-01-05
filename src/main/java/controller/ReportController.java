ae2dc6223b7faad17d94571af98656b9753dfcc6
rash-ad <Rashad.mohd2004@gmail.com>
2026-01-05 20:58:43 +0530

    added components in report page file in fxml and also in report controller

--- src/main/java/controller/ReportController.java
+++ src/main/java/controller/ReportController.java
@@ -1,5 +1,89 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+
 public class ReportController {
+
+    @FXML
+    private DatePicker DateStudentReport;
+
+    @FXML
+    private JFXButton btnAttendance;
+
+    @FXML
+    private JFXButton btnCourses;
+
+    @FXML
+    private JFXButton btnDashBoard;
+
+    @FXML
+    private JFXButton btnExportReport;
+
+    @FXML
+    private JFXButton btnGenerateReport;
+
+    @FXML
+    private JFXButton btnLogout;
+
+    @FXML
+    private JFXButton btnReports;
+
+    @FXML
+    private JFXButton btnSettings;
+
+    @FXML
+    private JFXButton btnStudentList;
+
+    @FXML
+    private JFXComboBox<?> comboBoxStudentReports;
+
+    @FXML
+    void btnAttendanceOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCOursesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnExportReport(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGenerateReportOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReportsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSetttingsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentListOnAction(ActionEvent event) {
+
+    }
 
 }
